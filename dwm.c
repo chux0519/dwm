054527079646f1a99690326c064ccfa952ee680c
hexyoungs <chuxdesign@hotmail.com>
2020-10-21 12:26:39 +0800

    update padding

--- dwm.c
+++ dwm.c
@@ -698,10 +698,14 @@ void drawbar(Monitor *m) {
   drw_setscheme(drw, scheme[SchemeNorm]);
   x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
 
+  char left_gap[] = "|";
+  w = TEXTW(left_gap);
+  x = drw_text(drw, x, 0, w, bh, lrpad / 4, left_gap, 0);
+
   if ((w = m->ww - tw - x) > bh) {
     if (m->sel) {
-      drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
-      drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
+      drw_setscheme(drw, scheme[SchemeNorm]);
+      drw_text(drw, x, 0, w, bh, lrpad / 4, m->sel->name, 0);
       if (m->sel->isfloating)
         drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
     } else {
